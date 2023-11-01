33611121f6307a009c4449a09beb61ac8945b04e
joshh5790 <joshh5790@gmail.com>
2023-11-01 00:42:39 -0700

    complete product form

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -1,35 +1,25 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, URLField, BooleanField
-# from wtforms.validators import DataRequired, ValidationError
-# from app.models import User
+from flask_wtf import FlaskForm
+from wtforms import StringField, URLField
+from wtforms.validators import DataRequired, ValidationError
 
 
-# def mainImage_data(form, field):
-# 	mainImage = field.data
-# 	if not mainImage:
-# 		raise ValidationError('Main image field is required.')
+def name_data(form, field):
+	name = field.data
+	if not name:
+		raise ValidationError('Please enter a name.')
 
-# def isBanner_data(form, field):
-# 	isBanner = field.data
-# 	if not isBanner:
-# 		raise ValidationError('Please indicate if the main image is a banner or a full size image.')
+def price_data(form, field):
+	price = field.data
+	if not price:
+		raise ValidationError('Please enter a price.')
 
-# def videoSection_data(form, field):
-# 	videoSection = field.data
-# 	if not videoSection:
-# 		raise ValidationError('Please indicate if the video section is enabled or not.')
+def previewImage_data(form, field):
+	previewImage = field.data
+	if not previewImage:
+		raise ValidationError('Please enter a preview image.')
 
-# def shopSection_data(form, field):
-# 	shopSection = field.data
-# 	if not shopSection:
-# 		raise ValidationError('Please indicate if the shop section is enabled or not.')
-
-# class ReppForm(FlaskForm):
-#   mainImage = URLField('mainImage', [DataRequired()])
-#   isBanner = BooleanField('isBanner', [DataRequired()])
-#   mainVideo = URLField('mainVideo')
-#   bio = StringField('bio')
-#   newsletter = StringField('newsletter')
-#   businessInquiries = StringField('businessInquiries')
-#   videoSection = BooleanField('videoSection', [DataRequired()])
-#   shopSection = BooleanField('shopSection', [DataRequired()])
+class ReppForm(FlaskForm):
+  name = StringField('name', [DataRequired(), name_data])
+  description = StringField('description')
+  price = StringField('price', [DataRequired(), price_data])
+  previewImage = URLField('previewImage', [DataRequired(), previewImage_data])
