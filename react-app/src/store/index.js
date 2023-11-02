18ac943f9906a1d4b6b320994a13cd939181b238
joshh5790 <joshh5790@gmail.com>
2023-11-02 16:14:28 -0700

    add products to root reducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import session from './session'
 import pages from './pages'
+import products from './products'
 
 const rootReducer = combineReducers({
   session,
   pages,
+  products,
 })
 
 
