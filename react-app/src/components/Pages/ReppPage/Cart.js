4a9c3fd512ed39cd2dcf308c10c596c8c3efb66f
joshh5790 <joshh5790@gmail.com>
2023-11-06 17:34:39 -0800

    can view cart items

--- react-app/src/components/Pages/ReppPage/Cart.js
+++ react-app/src/components/Pages/ReppPage/Cart.js
@@ -4,13 +4,14 @@ import { NavLink, useHistory } from "react-router-dom";
 // import CartItemCard from '../CartItems/CartItemCard'
 import { deleteCartThunk, getPageCartThunk } from "../../../store/carts";
 import { setCartVisibility } from "../../../store/navigation";
+import { formatCurrency } from "../../../utilities";
 import "./Cart.css";
 import { getCartItemsThunk } from "../../../store/cartItems";
 
 const Cart = ({ pageId, numCartItems, setNumCartItems }) => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const cart = useSelector((state) => state.carts);
+  const cart = useSelector((state) => Object.values(state.carts)[0]);
   const cartItems = useSelector((state) => state.cartItems);
   const cartVisible = useSelector((state) => state.visibility.cart);
   // const [showOptions, setShowOptions] = useState(false);
@@ -22,6 +23,10 @@ const Cart = ({ pageId, numCartItems, setNumCartItems }) => {
       .then((data) => dispatch(getCartItemsThunk(data.id)))
       .then((data) => setNumCartItems(data.length));
   }, [dispatch, pageId]);
+
+  const updateCartItem = (quantity) => {
+
+  }
 
   return (
     <>
@@ -47,11 +52,39 @@ const Cart = ({ pageId, numCartItems, setNumCartItems }) => {
               className="fa-solid fa-x modal"
             />
             <div className="cart-sidebar-content">
-              <div>Cart</div>
+              <h2 style={{ marginTop: "0" }}>Cart</h2>
               <div className="cart-items-list">
-                {Object.values(cartItems).map(item => (
-                  <div>{item.name}</div>
+                {Object.values(cartItems).map((item) => (
+                  <div key={item.id} className="cart-item-card">
+                    <div className="flex" style={{ gap: "1rem" }}>
+                      <img className="cart-item-img" src={item?.image} />
+                      <div>
+                        <div style={{ marginBottom: "0.5rem" }}>
+                          {item?.name}
+                        </div>
+                        <div style={{ color: "#999999", fontSize: "0.9rem" }}>
+                          {item?.size}
+                        </div>
+                        <input />
+                      </div>
+                    </div>
+                    <div style={{ marginBottom: "0.5rem" }}>
+                      {formatCurrency(item?.price * item?.quantity)}
+                    </div>
+                  </div>
                 ))}
+              </div>
+              <div style={{position: "absolute", bottom: "0"}}>
+                <div className="subtotal">
+                  <span>Subtotal:</span>
+                  <span>{formatCurrency(cart?.subtotal)}</span>
+                </div>
+                <NavLink
+                to='/checkout'
+                onClick={() => dispatch(setCartVisibility(false))}
+                className='cart-checkout-button'>
+                Go to checkout
+              </NavLink>
               </div>
             </div>
           </div>
