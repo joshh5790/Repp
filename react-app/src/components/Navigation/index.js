9d1f494fd924c7d8c18c729f857b466ffba828b7
joshh5790 <joshh5790@gmail.com>
2024-01-03 14:18:19 -0800

    add about links

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -14,11 +14,11 @@ function Navigation() {
   const sessionUser = useSelector((state) => state.session.user);
   const navVisible = useSelector((state) => state.visibility.nav);
   const location = useLocation();
-  const [isHome, setIsHome] = useState(true)
+  const [isHome, setIsHome] = useState(true);
 
   useEffect(() => {
-    setIsHome(location.pathname === '/')
-  }, [location.pathname])
+    setIsHome(location.pathname === "/");
+  }, [location.pathname]);
 
   useEffect(() => {}, [sessionUser, dispatch]);
 
@@ -27,12 +27,18 @@ function Navigation() {
   };
 
   return (
-    <ul className={`nav ${navVisible ? "" : "nav-hidden"} ${isHome ? "home-nav" : ""}`}>
+    <ul
+      className={`nav ${navVisible ? "" : "nav-hidden"} ${
+        isHome ? "home-nav" : ""
+      }`}
+    >
       <li className="nav-left">
-        {sessionUser && <i
-          onClick={() => setSidebarVisible(true)}
-          className={`fa-solid fa-bars toggle-sidebar`}
-        />}
+        {sessionUser && (
+          <i
+            onClick={() => setSidebarVisible(true)}
+            className={`fa-solid fa-bars toggle-sidebar`}
+          />
+        )}
         <NavLink exact to="/" className="logo-link">
           <img src={logo} alt="repp" className="logo" />
         </NavLink>
@@ -41,35 +47,63 @@ function Navigation() {
             <div className="page-info">
               <h4>Home Page/Navbar</h4>
               <ul>
-                <li>Visiting artist page will cause navbar to disappear, clicking side ellipsis will make it show up</li>
+                <li>
+                  Visiting artist page will cause navbar to disappear, clicking
+                  side ellipsis will make it show up
+                </li>
                 <li>Click logo to return to home page</li>
-                <li>If user is logged in, following sidebar will be available next to the logo</li>
+                <li>
+                  If user is logged in, following sidebar will be available next
+                  to the logo
+                </li>
                 <li>Right side of navbar has login button/user settings</li>
                 <li>login for tiffany is demo@aa.io, password is password</li>
                 <li>site is fully mobile friendly</li>
               </ul>
             </div>
           )}
-          {location.pathname === '/profile/edit' && (
+          {location.pathname === "/profile/edit" && (
             <div className="page-info">
-            <h4>Edit Profile Page</h4>
-            <ul>
-              <li>4 tabs, general, socials, products, and videos</li>
-              <li>desktop view not yet available</li>
-            </ul>
+              <h4>Edit Profile Page</h4>
+              <ul>
+                <li>4 tabs, general, socials, products, and videos</li>
+                <li>desktop view not yet available</li>
+              </ul>
             </div>
           )}
-          {(location.pathname === '/tiffany-day' || location.pathname === '/highvyn' || location.pathname === '/eric-nam') && (
+          {(location.pathname === "/tiffany-day" ||
+            location.pathname === "/highvyn" ||
+            location.pathname === "/eric-nam") && (
             <div className="page-info">
-            <h4>Profile Page</h4>
-            <ul>
-              <li>clicking on artist name in profile navbar brings user to top</li>
-              <li>Cart is profile page specific, carts will only show for the respective artist's page</li>
-              <li>Newsletter functionality not yet available</li>
-            </ul>
+              <h4>Profile Page</h4>
+              <ul>
+                <li>
+                  clicking on artist name in profile navbar brings user to top
+                </li>
+                <li>
+                  Cart is profile page specific, carts will only show for the
+                  respective artist's page
+                </li>
+                <li>Newsletter functionality not yet available</li>
+              </ul>
             </div>
           )}
         </i>
+        <div
+          style={{
+            display: "flex",
+            gap: "1rem",
+            fontSize: "2rem",
+            marginLeft: "1rem"
+          }}
+        >
+          <a href="https://github.com/joshh5790">
+            <i className="fa-brands fa-github" style={{color: "white"}} />
+          </a>
+          <a href="https://www.linkedin.com/in/joshua-ho-5790/">
+            <i className="fa-brands fa-linkedin" style={{color: "white"}} />
+          </a>
+        </div>
       </li>
       {/* <li className="search-bar-container">
         <SearchBar />
