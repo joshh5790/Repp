62efdd3e909969e2b9d0a058be7b1b7a6c444d54
joshh5790 <joshh5790@gmail.com>
2024-03-10 23:45:27 -0700

    add profileLinks seed file

---
+++ app/seeds/profileLinks.py
@@ -0,0 +1,37 @@
+from app.models import db, ProfileLink, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+def seed_profileLinks():
+    db.session.add_all(
+        [
+            ProfileLink(
+                profileId=2,
+                text="APESHIT OUT NOW",
+                link="https://lnk.dmsmusic.co/tiffanyday_apeshit",
+            ),
+            ProfileLink(
+                profileId=2,
+                text="subscribe to my newsletter",
+                link="https://tiffany-day.ck.page/9381985c5c",
+            ),
+        ]
+    )
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_profileLinks():
+    if environment == "production":
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.profilelinks RESTART IDENTITY CASCADE;"
+        )
+    else:
+        db.session.execute(text("DELETE FROM profilelinks"))
+
+    db.session.commit()
