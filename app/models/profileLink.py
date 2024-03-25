d0badc3f17ccc26707346c2365670ead7e4dc0c3
joshh5790 <joshh5790@gmail.com>
2024-03-24 23:18:12 -0700

    style edit profile link section

--- app/models/profileLink.py
+++ app/models/profileLink.py
@@ -1,22 +1,28 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
+
 class ProfileLink(db.Model):
-  __tablename__ = 'profilelinks'
+    __tablename__ = "profilelinks"
 
-  if environment == "production":
-    __table_args__ = {'schema': SCHEMA}
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
 
-  id = db.Column(db.Integer, primary_key=True)
-  profileId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("profiles.id")), nullable=False)
-  text = db.Column(db.String(50), nullable=False)
-  link = db.Column(db.String(50), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    profileId = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("profiles.id")), nullable=False
+    )
+    text = db.Column(db.String(50), nullable=False)
+    link = db.Column(db.String(50), nullable=False)
 
-  profile = db.relationship("Profile", back_populates="profileLinks")
+    profile = db.relationship("Profile", back_populates="profileLinks")
 
-  def to_dict(self):
-    return {
-      'id': self.id,
-      'profileId': self.profileId,
-      'text': self.text,
-      'link': self.link,
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "profileId": self.profileId,
+            "text": self.text,
+            "link": self.link,
+        }
+
+    def get_profileOwnerId(self):
+        return self.profile.to_dict()["userId"]
--- react-app/src/components/Pages/EditProfile/EditProfileLinks.js
+++ react-app/src/components/Pages/EditProfile/EditProfileLinks.js
@@ -3,18 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   deleteProfileLinkThunk,
   getProfileLinksThunk,
-  updateProfileLinkThunk,
 } from "../../../store/profileLinks";
-// import AddProfileLink from "../../Modals/AddProfileLink";
+import ManageProfileLink from "../../Modals/ManageProfileLink";
 import OpenModalButton from "../../OpenModalButton";
 import "./EditProfileLinks.css";
 
 const EditProfileLinks = ({ profile }) => {
   const dispatch = useDispatch();
-  const profileLinks = useSelector((state) => Object.values(state.profileLinks)); // .sort((a, b) => a.date - b.date), profileLink dates are not date variables
-  const [text, setText] = useState("");
-  const [link, setLink] = useState("");
-  const [editInput, setEditInput] = useState(0);
+  const profileLinks = useSelector((state) =>
+    Object.values(state.profileLinks)
+  );
 
   useEffect(() => {
     if (profile) {
@@ -22,100 +20,47 @@ const EditProfileLinks = ({ profile }) => {
     }
   }, [dispatch, profile]);
 
-  const focusProfileLink = (e, profileLink) => {
-    e.preventDefault();
-    if (!editInput) {
-      setEditInput(profileLink.id);
-      setText(profileLink.text);
-      setLink(profileLink.link);
-    }
-  };
-
   const handleDeleteProfileLink = (profileLinkId, e) => {
-    e.preventDefault()
+    e.preventDefault();
     dispatch(deleteProfileLinkThunk(profileLinkId));
-    resetState();
-  };
-
-  const handleUpdateProfileLink = (profileLinkId) => {
-    dispatch(
-      updateProfileLinkThunk({ profileLinkId, text, link })
-    );
-    resetState();
-  };
-
-  const resetState = () => {
-    setEditInput(0);
-    setText("");
-    setLink("");
   };
 
   return (
     <>
-      {/* <OpenModalButton
-        modalComponent={<AddProfileLink profileId={profile?.id}/>}
+      <OpenModalButton
+        modalComponent={<ManageProfileLink profileId={profile?.id} />}
         buttonText={<b>+ New Link</b>}
         className={"new-card-button"}
-      /> */}
-      <div className="edit-profileLinks-list">
+      />
+      <div className="edit-profilelinks-list flex-col">
         {profileLinks &&
           profileLinks.map((profileLink) => (
-            <form
-              key={profileLink?.id}
-              className={`profileLink-card ease-bg ${
-                editInput === profileLink?.id ? "focus-profileLink" : ""
-              }`}
-            >
-              <button
-                className="edit-card"
-                style={{
-                  position: "absolute",
-                  top: "5px",
-                  right: "40px",
-                  alignSelf: "start",
-                  justifySelf: "end",
-                }}
-                onClick={(e) => focusProfileLink(e, profileLink)}
+            <div key={profileLink?.id} className="profilelink-card">
+              <a
+                target="_blank"
+                className="profilelink-text button-hover"
+                style={{ textDecoration: "none" }}
+                href={profileLink?.link}
               >
-                <i className="fa-regular fa-pen-to-square" />
-              </button>
-              <button
-                className="delete-card delete-profilelink"
-                onClick={(e) => handleDeleteProfileLink(profileLink.id, e)}
-              >
-                <i className="fa-solid fa-x" />
-              </button>
-              <input
-                className="profilelink-input"
-                placeholder="Text"
-                value={editInput !== profileLink.id ? profileLink.text : text}
-                onChange={(e) => setText(e.target.value)}
-                disabled={editInput !== profileLink.id}
-              />
-              {editInput === profileLink.id && (
-                <div
-                  style={{
-                    marginTop: "0.5rem",
-                    gridArea: "buttons",
-                    display: "flex",
-                    gap: "1rem",
-                  }}
+                {profileLink?.text}
+              </a>
+              <div>
+                <OpenModalButton
+                  className="edit-card ease-bg"
+                  buttonText={<i className="fa-regular fa-pen-to-square" />}
+                  modalComponent={
+                    <ManageProfileLink profileLink={profileLink} />
+                  }
+                />
+                <button
+                  className="delete-card delete-profilelink"
+                  onClick={(e) => handleDeleteProfileLink(profileLink.id, e)}
+                  style={{ justifySelf: "flex-end" }}
                 >
-                  <button
-                    className="profilelink-button-cancel button-hover"
-                    onClick={resetState}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    className="profilelink-button-save button-hover"
-                    onClick={() => handleUpdateProfileLink(profileLink.id)}
-                  >
-                    Save
-                  </button>
-                </div>
-              )}
-            </form>
+                  <i className="fa-solid fa-x" />
+                </button>
+              </div>
+            </div>
           ))}
       </div>
     </>
